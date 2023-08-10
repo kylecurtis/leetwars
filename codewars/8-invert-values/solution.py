7d1a1b56bcba783a22f43da9f399b5aa2f31204a
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-08-10 02:22:57 -0400

    Add invert-values

---
+++ codewars/8-invert-values/solution.py
@@ -0,0 +1,12 @@
+# SOLUTION
+def invert(lst):
+    invert_lst = []
+    for num in lst:
+        invert_lst.append(-num)
+    return invert_lst
+
+# TEST FUNCTION
+def test_invert():
+    assert invert([1, 2, 3, 4, 5]) == [-1, -2, -3, -4, -5]
+    assert invert([1, -2, 3, -4, 5]) == [-1, 2, -3, 4, -5]
+    assert invert([]) == []
