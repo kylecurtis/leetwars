29343cd5b273058bd6a47aae5d04df311c4a8f48
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-08-10 02:14:19 -0400

    Add remove-duplicates-from-list

---
+++ codewars/8-remove-duplicates-from-list/solution.py
@@ -0,0 +1,16 @@
+# SOLUTION
+def distinct(seq):
+    clean = []
+    for x in seq:
+        if x not in clean:
+            clean.append(x)
+    return clean
+
+# TEST FUNCTION
+def test_distinct():
+    assert distinct([1]) == [1]
+    assert distinct([1, 2]) == [1, 2]
+    assert distinct([1, 1, 2]) == [1, 2]
+    assert distinct([1, 1, 1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    assert distinct([1, 2, 2, 3, 3, 4, 4, 5, 6, 7, 7, 7]) == [1, 2, 3, 4, 5, 6, 7]
+
