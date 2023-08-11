26cfec6fb291080c2cd5f3a0b61a6bd536c1221b
Kyle Curtis <30717363+KyleCurtis@users.noreply.github.com>
2023-08-11 00:56:36 -0400

    Add square-n-sum

---
+++ codewars/8-square-n-sum/solution.py
@@ -0,0 +1,9 @@
+def square_sum(numbers):
+    return(sum(x ** 2 for x in numbers))
+
+def test_square_sum():
+    assert square_sum([1, 2]) == 5
+    assert square_sum([0, 3, 4, 5]) == 50
+    assert square_sum([]) == 0
+    assert square_sum([-1, -2]) == 5
+    assert square_sum([-1, 0, 1]) == 2
