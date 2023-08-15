fe9aa9dce018a6ffc40725e804d44eb85aab8ba9
Kyle Curtis <30717363+kylecurtis@users.noreply.github.com>
2023-08-14 20:43:12 -0400

    Add remove-first-and-last-character

---
+++ codewars/8-remove-first-and-last-character/solution.py
@@ -0,0 +1,13 @@
+# SOLUTION
+def remove_char(s):
+    return s[1:-1]
+
+
+# TEST FUNCTION
+def test_remove_char():
+    assert remove_char("eloquent") == "loquen"
+    assert remove_char("country") == "ountr"
+    assert remove_char("person") == "erso"
+    assert remove_char("place") == "lac"
+    assert remove_char("ok") == ""
+    assert remove_char("ooopsss") == "oopss"
