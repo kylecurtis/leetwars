523ca39471a48ef5559c602a14fcfa543047dc38
Kyle Curtis <30717363+kylecurtis@users.noreply.github.com>
2023-08-27 23:53:23 -0400

    Add even-or-odd

---
+++ codewars/8-even-or-odd/solution.py
@@ -0,0 +1,18 @@
+# SOLUTION
+def even_or_odd(number):
+    return "Even" if number % 2 == 0 else "Odd"
+
+
+# TEST FUNCTION
+def test_even_or_odd():
+    assert even_or_odd(2) == "Even"
+    assert even_or_odd(1) == "Odd"
+    assert even_or_odd(0) == "Even"
+    assert even_or_odd(1545452) == "Even"
+    assert even_or_odd(7) == "Odd"
+    assert even_or_odd(78) == "Even"
+    assert even_or_odd(17) == "Odd"
+    assert even_or_odd(74156741) == "Odd"
+    assert even_or_odd(100000) == "Even"
+    assert even_or_odd(-123) == "Odd"
+    assert even_or_odd(-456) == "Even"
